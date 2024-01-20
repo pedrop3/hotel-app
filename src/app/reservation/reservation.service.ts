212abfd61e06b735d9acc44bd1ce6ee6526d5fde
Pedro Santos <pedro.santos@imedicina.com.br>
2024-01-20 01:46:13 +0000

    creating the reservation service

--- src/app/reservation/reservation.service.ts
+++ src/app/reservation/reservation.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
+import { Reservation } from '../models/reservation';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ReservationService {
 
-  constructor() { }
+
+  private reservations: Reservation[] = [];
+
+  findAll(): Reservation[] {
+    return this.reservations;
+  }
+
+  findById(id: string): Reservation | undefined {
+    return this.reservations.find(res => res.id === id)
+  }
+
+  deleteById(id: string): void {
+    let index = this.reservations.findIndex(res => res.id === id)
+    this.reservations.splice(index, 1);
+  }
+
+  save(reservation: Reservation): void {
+    let index = this.reservations.findIndex(res => res.id == reservation.id)
+
+  }
 }
